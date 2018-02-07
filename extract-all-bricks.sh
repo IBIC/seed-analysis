887424619fb2a57687d1c6e5e4af3d6a816a8e9c
TrevorKMDay <tkmday@uw.edu>
2018-02-07 10:52:08 -0800

    add brik extraction to repo

---
+++ extract-all-bricks.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Check that there are input files
+if [ ${#} -lt 2 ] ; then
+	echo "Must input at least one BRIK file!"
+	exit 1
+fi
+
+# Get files
+outputdir=${1} ; shift
+inputfiles=${@}
+
+mkdir -p ${outputdir}
+
+# Loop over each of the input files
+for ifile in ${inputfiles}
+do
+	# Skip this file if it isn't a brik file
+	if [[ ! ${ifile} =~ .*BRIK ]]; then
+		>&2 echo "${ifile} isn't a BRIK file!"
+		continue
+	fi
+
+	# Get the max brick index (N - 1)
+	maxbrikindex=$(3dinfo -nvi ${ifile})
+
+	# Get label array
+	labels=($(echo $(3dinfo -label ${ifile} | sed 's/|/ /g')))
+
+	# ROI
+	roi=$(basename ${ifile} | sed 's/\+.*.BRIK//')
+
+	# Extract the brick with the label for that brik
+	# Is there a better way to do this?
+	for i in $(seq 0 ${maxbrikindex})
+	do
+	 	3dAFNItoNIFTI \
+			-prefix ${outputdir}/${roi}_${labels[$i]}.nii.gz \
+			${ifile}[${i}]
+	done
+
+
+done
