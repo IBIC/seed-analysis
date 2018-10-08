7e8a5b877d4a3b239708b60e954c844a09ced2c4
TrevorKMDay <tkmday@uw.edu>
2018-10-08 10:03:28 -0700

    rewrite picture-taking

--- bin/slice-all-that-match.sh
+++ bin/slice-all-that-match.sh
@@ -25,16 +25,44 @@ for i in ${input} ; do
 		echo "${bn}: Overlay image not created, input must be empty"
 		exit
 	else
-		# slices \
-		# 	${output}-overlay.nii.gz \
-		# 	-o ${output}.gif
 
-		# Slicer outputs png, not gif
-		slicer \
-			${output}-overlay.nii.gz \
-			-a ${output}.png
+		# Get the mean of the image, removing whitespace
+		clustersmean=$(fslstats ${i} -m | tr -d '[:space:]')
+
+		# If there are clusters (i.e. the image mean is not 0), then take a
+		# picture of the overlay
+		if [[ ${clustersmean} != "0.000000" ]] ; then
+
+			slicer \
+				${output}-overlay.nii.gz \
+				-a ${output}.png
+
+		else
+
+			temp=$(mktemp XXXX.png)
+
+			# Otherwise, take a picture of the MNI template for placeholding
+			# purposes
+			slicer \
+				${FSLDIR}/data/standard/MNI152_T1_2mm_brain.nii.gz \
+				-a ${temp}
+
+			convert \
+				${temp} \
+				-gravity   center \
+				-pointsize 30     \
+				-fill      red    \
+				-annotate  +0+0 "EMPTY" \
+				${output}.png
+
+			# Clean up
+			rm ${temp}
+
+		fi
+
 	fi
 
+	# Clean up
 	rm -f $(dirname ${i})/${bn}-overlay.nii.gz
 
 done
