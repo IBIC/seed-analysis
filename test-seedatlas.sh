4065c7ce8ddd40e4e26b85dd2ab9d29013524588
Trevor <tkmday@uw.edu>
2018-11-27 17:44:55 -0800

    recluster

--- test-seedatlas.sh
+++ test-seedatlas.sh
@@ -3,7 +3,4 @@
 # Tara wants the NBM, l/r PPN, l/r putamen, and r hippocampus
 
 bin/seed-atlas.sh \
-	hcOn-pdOff/clustcorr/*NBMNBM4*_sai_*oindex.nii.gz \
-	hcOn-pdOff/clustcorr/*PPN*_sai_*oindex.nii.gz \
-	hcOn-pdOff/clustcorr/*putamen*_sai_*oindex.nii.gz \
-	hcOn-pdOff/clustcorr/*RHC*_sai_*oindex.nii.gz \
+	pdOn-pdOff/clustcorr/*{NBMNBM4,PPN,putamen,RHC}*_sai_*clusters.nii.gz
